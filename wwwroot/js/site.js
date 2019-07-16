f5d273fc80e4cf78b7c53d71d7c6e1117555fb3a
abdulMunnim <munnim9711511@gmail.com>
2019-07-16 14:38:57 +0500

    test

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,22 +5,45 @@
 
 
 $(() => {
+    var oldData;
     var connection = new signalR.HubConnectionBuilder().withUrl("/hub-token").build();
     connection.on("token", (data) => {
+        oldData = data;
 
        
         $("#bodyT").empty();
      
 
         for (x of data.Queue) {
-            $("#bodyT").append(
 
-                `<tr><td class=left-align><h3 onChange="test()" style=font-weight:bolder class=teal-text >${x.RoomLabel}</h3></td ><td  class=center-align ><h3 class=teal-text text-darken-4 style=font-weight:bolder >${x.TokenNo}</h3></td></tr>`
 
-            
+            for (xc of oldData.Queue) {
+                if (x.RoomLabel == xc.RoomLabel) {
+                    $("#bodyT").append(
 
-            ).addClass("#1de9b6 teal accent - 1 notis");
-            $("#bodyT h3").addClass("text-darken-4");
+                        `<tr><td class=left-align><h3 onChange="test()" style=font-weight:bolder  >${x.RoomLabel}</h3></td ><td  class=center-align ><h3  style=font-weight:bolder >${x.TokenNo}</h3></td></tr>`
+
+
+
+                    ).addClass("#1de9b6 teal accent - 1 notis");
+                    $("#bodyT h3").addClass("red-text");
+                    break;
+
+                }
+                else {
+                    $("#bodyT").append(
+
+                        `<tr><td class=left-align><h3 onChange="test()" style=font-weight:bolder class=teal-text >${x.RoomLabel}</h3></td ><td  class=center-align ><h3 class=teal-text text-darken-4 style=font-weight:bolder >${x.TokenNo}</h3></td></tr>`
+
+
+
+                    ).addClass("#1de9b6 teal accent - 1 notis");
+                    $("#bodyT h3").addClass("text-darken-4");
+                }
+                break;
+            }
+           
+         
            
         }
         var c = $("#op").html();
